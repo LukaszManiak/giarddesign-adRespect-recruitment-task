dc2334e07176538e605224d09e1bcf127793877b
Lukasz <lukasz.maniak22@gmail.com>
2023-08-20 23:10:52 +0200

    main

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        lightBrown: "#DCC1AB",
+        creamyWhite: "#F5F0EC",
+        green: "#1B5B31",
+        lightBlack: "#111111",
+      },
+    },
+  },
+  plugins: [],
+};
